e104cd6b94c7cd95e403a74f4f77544c54c18602
Thomas Alfa Edison <thomas.alfa.edison@gmail.com>
2018-07-26 13:46:30 +0700

    Update

--- themes/adminlte/views/layouts/frontend/navbar.php
+++ themes/adminlte/views/layouts/frontend/navbar.php
@@ -19,16 +19,10 @@ use app\models\User;
         ['label' => 'Tentang Kami','url'=>['site/about']],
         ['label' => 'Berita','url'=>['site/index']],
         ['label' => 'Artikel','url'=>['site/index']],
-<<<<<<< HEAD
         ['label' => 'Publikasi','url'=>['site/index']],
-        ['label' => 'Kontak Kami','url'=>['site/index']],
-        //['label' => 'Logout','url' => ['site/logout'], 'options' => ['data-method' => 'post'], 'visible' => !Yii::$app->user->isGuest],
-        //['label' => 'Login','url' => ['site/login'], 'visible' => Yii::$app->user->isGuest],
-=======
         ['label' => 'Kontak Kami','url'=>['site/contact']],
         ['label' => 'Logout','url' => ['site/logout'], 'options' => ['data-method' => 'post'], 'visible' => !Yii::$app->user->isGuest],
         ['label' => 'Login','url' => ['site/login'], 'visible' => Yii::$app->user->isGuest],
->>>>>>> 20b890f8b1a632939675dd2179d502a62ce2c02e
     ],
     'options' => ['class' => 'navbar-nav pull-left'],
 ]); ?>
